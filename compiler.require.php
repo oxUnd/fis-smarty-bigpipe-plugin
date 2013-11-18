5e6713ce2ca23004960603ae2a2fc962e402af36
xiangshouding <fansekey@gmail.com>
2013-11-18 16:56:35 +0800

    统一代码

--- compiler.require.php
+++ compiler.require.php
@@ -2,25 +2,21 @@
 
 function smarty_compiler_require($arrParams,  $smarty){
     $strName = $arrParams['name'];
-    //async，组件进行异步加载，针对JS
-    $strAsync = $arrParams['async'];
+    
+    $async = 'false';
 
-    if ($strAsync) {
-        $strAsync = trim($strAsync, '\'"');
-        if ($strAsync == 'true') {
-            $strAsync = 'true';
-        } else {
-            $strAsync = 'false';
+    if (isset($arrParams['async'])) {
+        $async = trim($arrParams['async'], "'\"");
+        if ($async !== 'true') {
+            $async = 'false';
         }
-    } else {
-        $strAsync = 'false';
     }
 
     $strCode = '';
     if($strName) {
         $strResourceApiPath = preg_replace('/[\\/\\\\]+/', '/', dirname(__FILE__) . '/lib/FISPagelet.class.php');
         $strCode .= '<?php if(!class_exists(\'FISPagelet\', false)){require_once(\'' . $strResourceApiPath . '\');}';
-        $strCode .= 'FISPagelet::load(' . $strName . ',$_smarty_tpl->smarty,'.$strAsync.');';
+        $strCode .= 'FISPagelet::load(' . $strName . ',$_smarty_tpl->smarty,'.$async.');';
         $strCode .= '?>';
     }
     return $strCode;
