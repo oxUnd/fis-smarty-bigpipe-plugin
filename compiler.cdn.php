287fb0a6c9cd6a1a51e34ce02ae2204f37c3cdcb
xiangshouding <fansekey@gmail.com>
2013-11-29 09:44:48 +0800

    update

--- compiler.cdn.php
+++ compiler.cdn.php
@@ -3,10 +3,10 @@
 function smarty_compiler_cdn($params, $smarty) {
 	$cdn = isset($params['domain']) ? $params['domain'] : '';
 	$strCode = '';
-	if (class_exists('FISPagelet')) {
-		$strCode .= '<?php ';
-		$strCode .= 'FISPagelet::setCdn('.$cdn.');';
-		$strCode .= ' ?>';
-	}
+	$strCode .= '<?php ';
+	$strCode .= 'if (class_exists("FISPagelet", false)) {'
+	$strCode .= 'FISPagelet::setCdn('.$cdn.');';
+	$strCode .= '}';
+	$strCode .= ' ?>';
 	return $strCode;
 }
