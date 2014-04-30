8ee51bb35e1b9136fdc2261190f108800bf911a9
xiangshouding <fansekey@gmail.com>
2014-04-30 17:15:19 +0800

    ...

--- lib/FISPagelet.class.php
+++ lib/FISPagelet.class.php
@@ -1,7 +1,5 @@
 <?php
-if (!class_exists('FISResource')) require_once(dirname(__FILE__) . '/FISResource.class.php');
-
-
+if (!class_exists('FISResource', false)) require_once(dirname(__FILE__) . '/FISResource.class.php');
 
 /**
  * Checks a string for UTF-8 encoding.
